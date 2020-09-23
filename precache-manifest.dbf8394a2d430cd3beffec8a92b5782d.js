607b19a50730bae189e70f49679c063d46d1430d
mukjo96 <mukjo96@gmail.com>
2020-09-23 17:23:34 +0900

    Updates

---
+++ precache-manifest.dbf8394a2d430cd3beffec8a92b5782d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3d99466add0c5dccd38bb13fa255f65",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "b9b02190629d026ab17c",
+    "url": "/nwitter/static/css/main.04e6e294.chunk.css"
+  },
+  {
+    "revision": "932ac42193a30ab88a68",
+    "url": "/nwitter/static/js/2.b7d68c96.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/nwitter/static/js/2.b7d68c96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9b02190629d026ab17c",
+    "url": "/nwitter/static/js/main.39ba90d3.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
